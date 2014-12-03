e1ab91a21c61ce4646f3528584708be0d29864a2
Roel Gelink <roel.gelink@gmail.com>
2014-12-03 14:41:51 +0100

    Changed ProjectController
    Added Edit/Delete functionality

--- public/controllers/project.js
+++ public/controllers/project.js
@@ -8,14 +8,19 @@
     .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/addproject', {
-                        templateUrl: '/views/addproject.html',
-                        controller: 'AddProjectController',
-                    })
+                templateUrl: '/views/addproject.html',
+                controller: 'AddProjectController'
+            })
             .when('/projects', {
                 templateUrl: '/views/projects.html',
-                controller: 'ViewProjectsController',
+                controller: 'ViewProjectsController'
             })
+            .when('/editproject', {
+                templateUrl: '/views/editproject.html',
+                controller: 'EditProjectController'
+            });
 
+        $locationProvider.html5Mode(true);
     }])
     .controller('AddProjectController', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams) {
         $scope.addProject = function () {
@@ -37,6 +42,37 @@
         .error (function (data){
             console.log("error: " + data);
         });
+    }])
+    .controller('EditProjectController', ['$http', '$scope', '$routeParams', '$location', function($http, $scope, $routeParams, $location) {
+        var id = $routeParams._id;
+        
+        $http.get('/api/projects/' + id) 
+            .success (function (data) {
+                $scope.project=data;
+            })
+            .error (function (data){
+                console.log("error: " + data);
+            });
+
+        $scope.deleteProject = function () {
+            $http.delete('/api/projects/' + id)
+                .success (function (data) {
+                    $location.path('/projects');
+                })
+                .error (function (data){
+                    console.log("error: " + data);
+                });
+        };
+
+        $scope.updateProject = function () {
+            $http.put('/api/projects/' + id, $scope.project)
+                .success (function (data) {
+                    $location.path('/projects');
+                })
+                .error (function (data){
+                    console.log("error: " + data);
+                });
+        };
     }]);
 })();
 
