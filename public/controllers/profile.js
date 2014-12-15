59d5bc5843e7f30c6192f230faa2acb93f912f43
Roel Gelink <roel.gelink@gmail.com>
2014-12-15 10:09:42 +0100

    Added a view to edit a profile
    Contains the view and included form to edit a profile. Missing the
    functionality

--- public/controllers/profile.js
+++ public/controllers/profile.js
@@ -12,6 +12,11 @@
                     templateUrl: '/views/profile.html',
                     controller: 'ProfileController',
                     controllerAs: 'profile'
+                })
+                .when('/editprofile', {
+                    templateUrl: '/views/editprofile.html',
+                    controller: 'EditProfileController',
+                    controllerAs: 'editprofile'
                 });
 
             $locationProvider.html5Mode(true);
@@ -22,8 +27,22 @@
                 .success(function(data) {
                     $scope.user = data; //Expose the user data to your angular scope
                 });
+        }])
 
-
+        .controller('EditProfileController', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams) {
+            //Custom Profile functionality
+            $http.get('/api/userData')
+                .success(function(data) {
+                    $scope.user = data; //Expose the user data to your angular scope
+                });
+            $http.get('/api/profiles')
+                .success (function (data) {
+                    console.log(data);
+                    $scope.profiles = data;
+                });
+            $http.get('/api/semesters')
+                .success (function (data) {
+                    $scope.semesters = data;
+                }); 
         }]);
-
 })();
