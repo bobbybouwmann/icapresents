05a761117ee243c11e73e824ec83318df23d5af7
pjotrcasteel <pjotr_2@live.nl>
2014-12-16 12:06:58 +0100

    Small fixes

--- public/js/fly-out-menu.js
+++ public/js/fly-out-menu.js
@@ -45,3 +45,18 @@
 $(document).on('click', '.remove-layout', function () {
     $(this).parent().remove();
 });
+
+$('#colorSelector').ColorPicker({
+    color: '#0000ff',
+    onShow: function (colpkr) {
+        $(colpkr).fadeIn(500);
+        return false;
+    },
+    onHide: function (colpkr) {
+        $(colpkr).fadeOut(500);
+        return false;
+    },
+    onChange: function (hsb, hex, rgb) {
+        $('#colorSelector div').css('backgroundColor', '#' + hex);
+    }
+});
