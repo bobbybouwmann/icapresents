823a18a42ceb6623e4abb5bcea044f1cd407935f
Bobby Bouwmann <bobbybouwmann@gmail.com>
2014-12-16 10:39:39 +0100

    Added side menus + adding layouts
    - Added directive to open the side menu
    - Updated add project page

---
+++ public/js/fly-out-menu.js
@@ -0,0 +1,47 @@
+ $(document).ready(function() { 
+    
+    $('.menu-toggle').bind('click', function () {
+        $('body').toggleClass('menu-open');
+    });
+
+    $('.menu-toggle-right').bind('click', function () {
+        $('body').toggleClass('menu-open-right');
+    });
+
+    $('.add-layout-image').click(function () {
+        var $newdiv = $('<div class="layout layout-image row"><div class="remove-layout"><i class="fa fa-times"></i></div><img src="http://lorempixel.com/800/250"></div>');
+
+        $('#project-layouts').append($newdiv);
+        $('body').removeClass('menu-open');
+        $('body').removeClass('menu-open-right');
+    });
+
+    $('.add-layout-image-text').click(function () {
+        var $newdiv = $('<div class="layout layout-image-text row"><div class="remove-layout"><i class="fa fa-times"></i></div><div class="image col-xs-12 col-sm-6"><img src="http://lorempixel.com/400/250"></div><div class="text col-xs-12-col-sm-6"><h2>Lorem Pixel</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, quae rerum, recusandae fugit incidunt odit vitae non aliquid doloribus sequi pariatur sapiente, ipsa aspernatur unde aperiam quis culpa porro consequatur.</p></div></div>');
+
+        $('#project-layouts').append($newdiv);
+        $('body').removeClass('menu-open');
+        $('body').removeClass('menu-open-right');
+    });
+
+    $('.add-layout-text-image').click(function () {
+        var $newdiv = $('<div class="layout layout-text-image row"><div class="remove-layout"><i class="fa fa-times"></i></div><div class="text col-xs-12 col-sm-6"><h2>Lorem Pixel</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, quae rerum, recusandae fugit incidunt odit vitae non aliquid doloribus sequi pariatur sapiente, ipsa aspernatur unde aperiam quis culpa porro consequatur.</p></div><div class="image col-xs-12 col-sm-6"><img src="http://lorempixel.com/400/250"></div></div>');
+
+        $('#project-layouts').append($newdiv);
+        $('body').removeClass('menu-open');
+        $('body').removeClass('menu-open-right');
+    });
+
+    $('.add-layout-video').click(function () {
+        var $newdiv = $('<div class="layout layout-video row"><div class="remove-layout"><i class="fa fa-times"></i></div><div class="embed-container"><iframe src="http://www.youtube.com/embed/WmVXJ3hQrPo" frameborder="0" allowfullscreen></iframe></div></div>');
+
+        $('#project-layouts').append($newdiv);
+        $('body').removeClass('menu-open');
+        $('body').removeClass('menu-open-right');
+    });
+
+});
+
+$(document).on('click', '.remove-layout', function () {
+    $(this).parent().remove();
+});
